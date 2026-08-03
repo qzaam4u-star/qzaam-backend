adc483a70fb7305abb6c8336803504b94892afd5
Qzaam <qzaam4u@gmail.com>
2026-08-03 22:29:48 +0530

    Create couponController.js

---
+++ src/controllers/couponController.js
@@ -0,0 +1,28 @@
+const prisma = require("../prisma");
+
+exports.getCoupons = async (req, res) => {
+
+    const { vendorId } = req.query;
+
+    const coupons = await prisma.coupon.findMany({
+
+        where:{
+            active:true,
+            OR:[
+                {vendorId:null},
+                {vendorId}
+            ]
+        },
+
+        orderBy:{
+            createdAt:"desc"
+        }
+
+    });
+
+    res.json({
+        success:true,
+        data:coupons
+    });
+
+};
