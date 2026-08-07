3b2cf5db58924408df89d4651b2c3969b83e8bad
Qzaam <qzaam4u@gmail.com>
2026-08-07 16:50:22 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,13 +1,25 @@
 require("dotenv").config();
+
 const app = require("./app");
-require('./utils/archiveexpiredoffers');
+
+const { archiveExpiredOffers } = require("./utils/archiveexpiredoffers");
+const { checkOrderTimeouts } = require("./utils/orderTimeoutChecker");
+
 const PORT = process.env.PORT || 5000;
+
+// Run once when server starts
+archiveExpiredOffers();
+
+// Archive expired offers every 15 minutes
+setInterval(() => {
+  archiveExpiredOffers();
+}, 15 * 60 * 1000);
+
+// Existing order timeout checker
+setInterval(() => {
+  checkOrderTimeouts();
+}, 30000);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
-
-const { checkOrderTimeouts } = require("./utils/orderTimeoutChecker");
-setInterval(() => {
-  checkOrderTimeouts();
-}, 30000); // every 30 seconds
