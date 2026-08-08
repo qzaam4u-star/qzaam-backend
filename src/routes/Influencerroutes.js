cc341f266ff83845ada75ab3282d4e4512a5911f
Qzaam <qzaam4u@gmail.com>
2026-08-09 00:31:45 +0530

    Create Influencerroutes.js

---
+++ src/routes/Influencerroutes.js
@@ -0,0 +1,56 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  protect,
+  restrictTo
+} = require("../middlewares/auth.middleware");
+
+const influencerController =
+  require("../controllers/influencer.controller");
+
+
+router.post(
+  "/profile",
+  protect,
+  restrictTo("influencer"),
+  influencerController.createProfile
+);
+
+
+router.get(
+  "/me",
+  protect,
+  restrictTo("influencer"),
+  influencerController.getMyProfile
+);
+
+
+router.put(
+  "/me",
+  protect,
+  restrictTo("influencer"),
+  influencerController.updateMyProfile
+);
+
+
+/*
+ADMIN ONLY
+*/
+router.get(
+  "/admin",
+  protect,
+  restrictTo("admin"),
+  influencerController.getAllInfluencers
+);
+
+
+router.get(
+  "/admin/:id",
+  protect,
+  restrictTo("admin"),
+  influencerController.getInfluencerById
+);
+
+module.exports = router;
